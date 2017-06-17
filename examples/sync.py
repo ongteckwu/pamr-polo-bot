59cd8f4a43e4aeecb730136dd931952cbd138be6
Teck wu Ong <teckwuong@Tecks-MacBook-Pro.local>
2017-06-17 16:23:37 +0800

    Finally

--- examples/sync.py
+++
@@ -1,9 +0,0 @@
-from app import SyncApp
-
-__author__ = 'andrew.shvv@gmail.com'
-
-API_KEY = "YOUR_API_KEY"
-API_SECRET = "YOUR_API_SECRET"
-
-app = SyncApp(api_key=API_KEY, api_sec=API_SECRET)
-app.logger.debug(app.public.return24hVolume())
